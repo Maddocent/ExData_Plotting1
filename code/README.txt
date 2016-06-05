df0b82a37cd82ffeb4e1ace5368dccce9bd4e36e
Marc Teunis <marc.teunis@hu.nl>
2016-06-05 17:04:30 +0200

    init

---
+++ code/README.txt
@@ -0,0 +1,2 @@
+This folder contains all the code to the fouth course for the Coursera Data Sceince Specialization: "Exploratory Data Analysis"
+Author: Marc A.T. Teunis, June 2016
---
+++ code/swirl_exercises.R
@@ -0,0 +1,21 @@
+################################################
+## SWIRL EXERCISES ##
+################################################
+
+# install.packages("RCurl")
+# install.packages("swirl")
+
+library(swirl)
+install_from_swirl("Exploratory Data Analysis")
+swirl()
+
+## Then, follow the menus and select the Exploratory Data Analysis 
+## course when given the option. 
+## For the first part of this course you should 
+## complete the following lessons:
+  
+## Principles of Analytic Graphs
+## Exploratory Graphs
+## Graphics Devices in R
+## Plotting Systems
+## Base Plotting System
